d1af4df597b7bdf78edfca2c5ce7770c7936bce9
Wilson de Oliveira Junior <shadow7001@outlook.com>
2020-02-17 12:08:47 -0300

    aprimorando sistema de pesquisa de arquivos

--- MediaCutter/Program.cs
+++ MediaCutter/Program.cs
@@ -13,34 +13,23 @@ namespace MediaCutter
     {
         static void Main(string[] args)
         {
-            Separar_Arquivos();
-        }
-        private void Juntar_Arquivos()
-        {
-            string[] files = new string[] {
-                    "75827614_1020729014938936_8791711126376427222_n.mp4",
-                    "72709672_177936833345686_3309992862822149750_n.mp4",
-                    "77178595_150753886323197_8625749720430450148_n.mp4"
-                };
-
-
-            using (var engine = new Engine())
+            var files = new System.IO.DirectoryInfo(@"C:\Users\wilson.deoliveira\Downloads\").GetFiles();
+            foreach (var file in files)
             {
-                //engine.
-
-
-
-
+                if (file.Extension == ".mp4")
+                {
+                    Separar_Arquivos(file);
+                }
             }
         }
-        private static void Separar_Arquivos()
+        private static void Separar_Arquivos(System.IO.FileInfo file)
         {
             using (var engine = new Engine())
             {
-                string file = @"D:\doc\mp4\HITS-2019\31-Rubel, ANAVITÓRIA - Partilhar.mp4";
-                var inputFile = new MediaFile { Filename = file };
+                var fileName = file.Name.Replace(file.Extension, string.Empty);
+                var inputFile = new MediaFile { Filename = file.FullName };
                 engine.GetMetadata(inputFile);
-                var outputName = @"C:\Users\wilso\Downloads\31_";
+                var outputName = @"C:\Users\wilson.deoliveira\Downloads\temp\";
                 var outputExtension = ".mp4";
                 double Duration = inputFile.Metadata.Duration.TotalMilliseconds;
                 double currentPosition = 0;
@@ -53,7 +42,7 @@ namespace MediaCutter
                     if (currentPosition > Duration) continue;
 
                     var options = new ConversionOptions();
-                    var outputFile = new MediaFile(outputName + counter.ToString("00") + outputExtension);
+                    var outputFile = new MediaFile(outputName + fileName + "-" +  counter.ToString("00") + outputExtension);
 
                     options.CutMedia(TimeSpan.FromMilliseconds(currentPosition), TimeSpan.FromSeconds(30));
 
